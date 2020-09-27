42774012d8dbd6d062d8eff831d27e3ec3fce587
Ryan Challen <ryanchallen@outlook.com>
2020-09-27 15:58:48 +1000

    Added new favicon, minor styling, back home link to results

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,15 @@ import zombichu from "../zombichu.jpg";
 export default function Home() {
   return (
     <div className='container'>
-      <h1 className='home-heading'>POKEPOCALYPSE</h1>
+      <h1 className='home-heading'>POKÉPOCALYPSE</h1>
       <img src={zombichu} className='App-logo' alt='zombie Pikachu' />
       <p>Just when you thought it was feeling less horrible to be in 2020...</p>
-      <p>Pokemon have entered our reality and are actually vicious and terrifying ghouls.</p>
+      <p>Pokemon have entered our reality and are now terrifying pokéghouls.</p>
       <p>
-        Oh and their unified mantra is <span className=''>"Gotta kill 'em all!"</span>
+        Oh and their unified mantra is <span className='red'>"Gotta kill 'em all!"</span>
       </p>
-      <Link role='button' to='/result' className='btn btn-lg btn-danger'>
-        Which undead fuzzball will be your doom?
+      <Link role='button' to='/result' className='btn btn-lg btn-danger button-buffer'>
+        Click to see which undead fuzzball will be your doom!
       </Link>
     </div>
   );
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Result() {
   return (
     <div>
-      <h1>RESULTS</h1>
+      <h1>OH!</h1>
+      <p>A wild XXXX has appeared!</p>
+      <Link to='/' className='btn btn-link'>
+        {" "}
+        Go Home
+      </Link>
     </div>
   );
 }
