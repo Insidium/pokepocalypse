1030a0bb4eaa5dfa04651133dd322d9fd24d65fc
Ryan Challen <ryanchallen@outlook.com>
2020-09-27 16:46:25 +1000

    Minor styling, kanto only pokemon function,  link out to "hope"

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 import zombichu from "../zombichu.jpg";
 
 export default function Home() {
+  function getRand() {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+      .then(response => response.json())
+      .then(allpokemon => console.log(allpokemon));
+  }
   return (
     <div className='container'>
-      <h1 className='home-heading'>POKÉPOCALYPSE</h1>
+      <h1 className='heading'>POKÉPOCALYPSE</h1>
       <img src={zombichu} className='App-logo' alt='zombie Pikachu' />
       <p>Just when you thought it was feeling less horrible to be in 2020...</p>
       <p>Pokemon have entered our reality and are now terrifying pokéghouls.</p>
@@ -13,7 +18,7 @@ export default function Home() {
         Oh and their unified mantra is <span className='red'>"Gotta kill 'em all!"</span>
       </p>
       <Link role='button' to='/result' className='btn btn-lg btn-danger button-buffer'>
-        Click to see which undead fuzzball will be your doom!
+        <strong>Click to see which undead fuzzball will be your doom!</strong>
       </Link>
     </div>
   );
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import zombichu from "../zombichu.jpg";
 
 export default function Result() {
   return (
     <div>
-      <h1>OH!</h1>
-      <p>A wild XXXX has appeared!</p>
-      <Link to='/' className='btn btn-link'>
-        {" "}
-        Go Home
+      <Link to='/' className='btn btn-lg btn-link d-flex go-back'>
+        {"<< "}
+        Run Back Home
       </Link>
+      <div>
+        <h1 className='heading'>Oh (no)!</h1>
+        <img src={zombichu} className='App-logo' alt='Random Pokemon attacker' />
+        <p className='exclaim'>A wild XXXX has appeared!</p>
+        <p>According to poké-folklore, this may be your only hope...</p>
+        <a
+          role='button'
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://www.youtube.com/watch?v=oHg5SJYRHA0'
+          className='btn btn-lg btn-outline-success button-buffer'
+        >
+          <strong>TRY ANCIENT DISTRACTION RITUAL</strong>
+        </a>
+      </div>
     </div>
   );
 }
